6177391616a07846e9ab0b67fdff423e56c92136
Luchito-Pc <lucianopalacio91@gmail.com>
2020-10-06 19:35:33 -0300

    Practica 3

---
+++ ListasOrdenadas/src/listasordenadas/Listas.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listasordenadas;
+
+import lista.Lista;
+import lista.ListaEncadenada;
+
+/**
+ *
+ * @author Luciano
+ */
+
+
+public interface Listas<T> {
+    
+    /**
+    * Dado un elemento y una lista, ingreso el elemento a la lista
+    * @return lista ordenada con el nuevo elemento
+    **/
+    public Lista ins(T elem, Lista l);
+
+    /**
+    * Dadas dos listas, retorna la union de ellas de forma ordenada
+	* @return lista ordenada con la union de l1 y l2
+    **/
+    public Lista union(Lista l1 , Lista l2);
+
+    /**
+    * Dado un elemento y una lista, retorna si el elemento pertenece a la lista
+    * @return si el elemento pertence a la lista
+    **/
+    public Boolean per(T elem , Lista l);
+
+    /**
+    * Dadas dos listas, retorna la intercion de ellas
+    * @return lista ordenada con la interseccion de l1 y l2
+    **/
+    public Lista inter(Lista l1, Lista l2);
+
+}
---
+++ ListasOrdenadas/src/listasordenadas/ListasOrdenadas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listasordenadas;
+
+import lista.Lista;
+import lista.ListaEncadenada;
+/**
+ *
+ * @author Luciano
+ */
+public class ListasOrdenadas<T> {
+    
+	ListaOrdenada<T> list;
+
+	public ListasOrdenadas(){
+		ListaOrdenada<T> 
+	}
+
+}
